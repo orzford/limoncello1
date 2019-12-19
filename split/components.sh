de0da10b9956756c496d0cf18fd7ae269303b0b8
dreamsbond <info@dreamsbond.com>
2019-12-19 12:28:12 +0800

    Update.

---
+++ split/components.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+BRANCHES='master develop'
+
+COMPONENTS=(
+    'components/Data/:git@github.com:orzford/limoncello-data.git'
+    'components/Flute/:git@github.com:orzford/limoncello-flute.git'
+)
